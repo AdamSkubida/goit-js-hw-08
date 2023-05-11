8ef48006e31bc6255b4fb26054658b81d6b09c1b
AdamSkubida <adasiow.s@gmail.com>
2023-05-11 23:46:52 +0200

    final/before/check

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+'use strict';
+
+import { galleryItems } from './gallery-items.js';
+
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
 
 const galleryList = document.querySelector('.gallery');
 
-const addImagesToGallery = galleryItems.reduce((acc, newImage) => {
-  return (
-    acc +
-    `<li class="gallery__item"><a class="gallery__link"><img src="${newImage.preview}" alt="${newImage.description}" class="gallery__image" data-source="${newImage.original}"></a></li>`
-  );
-}, '');
+let template = galleryItems
+  .map(
+    item =>
+      `<li class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a></li>`
+  )
+  .join('');
 
-galleryList.insertAdjacentHTML('beforeend', addImagesToGallery);
+galleryList.insertAdjacentHTML('beforeend', template);
 
-galleryList.addEventListener('click', event => {
-  event.preventDefault();
-
-  const bigImg = event.target.dataset.source;
-  console.log(bigImg);
-
-  const bigAlt = event.target.alt;
-  console.log(bigAlt);
-
-  basicLightbox.create(`<img src="${bigImg}" alt="${bigAlt}">`).show();
+new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-console.log(galleryItems);
